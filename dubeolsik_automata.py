8b82d015fdea5b09e1fd15a34bc2b4e4b24f0551
Jihoon Kim <kjh618@kaist.ac.kr>
2020-08-25 15:44:24 +0900

    Add basic classes

--- dubeolsik_automata.py
+++ dubeolsik_automata.py
@@ -1,2 +1,30 @@
+from enum import Enum
+
+KEY_JAEUM = set('ㅂㅈㄷㄱㅅㅁㄴㅇㄹㅎㅋㅌㅊㅍㅃㅉㄸㄲㅆ')
+KEY_MOEUM = set('ㅛㅕㅑㅐㅔㅗㅓㅏㅣㅠㅜㅡㅒㅖ')
+
+class State(Enum):
+    START = 0
+    CHOSEONG = 1
+    JUNGSEONG1 = 2
+    JUNGSEONG2 = 3
+    JONGSEONG1 = 4
+    JONGSEONG2 = 5
+
+class CurrentCharacter:
+    def __init__(self):
+        self.choseong = None
+        self.jungseong = None
+        self.jongseong1 = None
+        self.jongseong2 = None
+
+    def join(self):
+        return '각'
+
 def join_jamos(jamos):
-    return '안녕하세요'
+    cur_state = State.START
+    result = []
+
+    for jamo in jamos:
+        if cur_state == State.START:
+            pass
