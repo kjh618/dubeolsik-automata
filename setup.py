57af031b387b337608151dd9124c4d365eff402e
Jihoon Kim <kjh618@kaist.ac.kr>
2020-08-26 15:21:03 +0900

    Fix Windows setup bug

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name='dubeolsik-automata',
-    version='0.1.0',
+    version='0.1.1',
     author='Jihoon Kim, Seongyeop Yi',
     author_email='kimjihoon6188@daum.net',
     description='Dubeolsik automata',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     url='https://github.com/kjh618/dubeolsik-automata',
-    download_url='https://github.com/kjh618/dubeolsik-automata/archive/v0.1.0.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
